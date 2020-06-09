65b17a88086da6ba95bf8a66d79f608bc4bc525a
tijani2020 <info.abderrahimtijani@gmail.com>
2020-06-09 22:38:49 +0200

    Enable descreption navigation with javascript

--- app.js
+++ app.js
@@ -23,3 +23,48 @@ else {
 
 
 });
+
+
+
+// Navigate information 
+
+const chooseInfo = document.getElementById('more-infos');
+const infoContent = document.getElementById('info-content');
+
+const firstTarget = document.getElementById('firstTarget');
+const secondTarget = document.getElementById('secondTarget');
+const thirdTarget = document.getElementById('thirdTarget');
+
+const firstchild = document.getElementById('firstchild');
+const secondchild = document.getElementById('secondchild');
+const thirdchild = document.getElementById('thirdchild');
+
+
+
+
+chooseInfo.addEventListener('click',function changeInfoContent (event) {
+
+
+    
+   if (event.target === firstTarget) {
+    firstchild.style.display = 'block';
+    secondchild.style.display = 'none';
+    thirdchild.style.display = 'none';
+    }
+    
+       else if (event.target === secondTarget) {
+        firstchild.style.display = 'none';
+        secondchild.style.display = 'block';
+        thirdchild.style.display = 'none';
+    
+       }
+    
+    else {
+        firstchild.style.display = 'none';
+        secondchild.style.display = 'none';
+        thirdchild.style.display = 'block';
+    }
+    
+
+});
+
