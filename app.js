cfba861c24ba91b00b82b783434196602cc38dc5
tijani2020 <info.abderrahimtijani@gmail.com>
2020-06-23 22:09:30 +0200

    First option default style

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // Navigate product images 
 
 const BigImage = document.getElementById('big-image');
-
-const Nomos1 = document.getElementById('Nomos1');
-const Nomos2 = document.getElementById('Nomos2');
-const Nomos3 = document.getElementById('Nomos3');
-
 const imgSlider = document.getElementById('img-slider');
 
 imgSlider.addEventListener('click', event => {
@@ -29,52 +24,39 @@ imgSlider.addEventListener('click', event => {
 
 const chooseInfo = document.getElementById('more-infos');
 const choose = document.getElementsByClassName('choose');
-
-const infoContent = document.getElementById('info-content');
 const paragraph = document.getElementsByClassName('paragraph');
+
+
+function styleItem (a,b,c) {
+    a.style.cssText = 'color:black ; border-bottom: 2px solid black ; padding-bottom: 6px';
+    b.style.cssText = 'color:gray ; border-bottom: none';
+    c.style.cssText = 'color:gray ; border-bottom: none';
+}
+
+function displayPrph (e,f,g) {
+        e.style.display = 'block';
+        f.style.display = 'none';
+        g.style.display = 'none';
+}
 
 chooseInfo.addEventListener('click', event => {
    
-    
-    for (i = 0 ; i <= paragraph.length ; i++) { 
-        
-
-       if (event.target === choose[0]) {          
-        
-        choose[0].style.cssText = 'color:black ; border-bottom: 2px solid black ; padding-bottom: 6px';
-        choose[2].style.cssText = 'color:gray ; border-bottom: none';
-        choose[1].style.cssText = 'color:gray ; border-bottom: none';
-
-        paragraph[0].style.display = 'block';
-        paragraph[2].style.display = 'none';
-        paragraph[1].style.display = 'none';
-
+       if (event.target === choose[0]) {    
+           
+                 styleItem (choose[0],choose[1],choose[2])
+                 displayPrph (paragraph[0],paragraph[2],paragraph[1])
         }  
 
-       else if (event.target === choose[1]) {   
-           
-        choose[1].style.cssText = 'color:black ; border-bottom: 2px solid black ; padding-bottom: 6px';
-        choose[0].style.cssText = 'color:gray ; border-bottom: none';
-        choose[2].style.cssText = 'color:gray ; border-bottom: none';
+       else if (event.target === choose[1]) {  
 
-        paragraph[1].style.display = 'block';
-        paragraph[0].style.display = 'none';
-        paragraph[2].style.display = 'none';
-
+                 styleItem (choose[1],choose[0],choose[2])
+                 displayPrph (paragraph[1],paragraph[0],paragraph[2])
         }  
 
        else   {
-        choose[2].style.cssText = 'color:black ; border-bottom: 2px solid black ; padding-bottom: 6px';
-        choose[0].style.cssText = 'color:gray ; border-bottom: none';
-        choose[1].style.cssText = 'color:gray ; border-bottom: none';
-       
-
-        paragraph[2].style.display = 'block';
-        paragraph[0].style.display = 'none';
-        paragraph[1].style.display = 'none';
-
-        }  
-    }  
+                  styleItem (choose[2],choose[0],choose[1])
+                  displayPrph (paragraph[2],paragraph[0],paragraph[1])
+        }     
 });
 
 // add items to the cart
